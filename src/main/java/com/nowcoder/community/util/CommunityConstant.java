5d5402885ef6d28b1e90e06b684eb9597969e22b
wzczichen <wzcwangzichen@foxmail.com>
2021-03-30 22:13:28 +0800

    2021-3-30

---
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -0,0 +1,27 @@
+package com.nowcoder.community.util;
+
+public interface CommunityConstant {
+
+    /*
+    激活成功
+     */
+    int ACTIVATION_SUCCESS = 0;
+    /*
+    重复激活
+     */
+    int ACTIVATION_REPEAT = 1;
+    /*
+    激活失败
+     */
+    int ACTIVATION_FALSE = 2;
+
+    /*
+    默认状态登录凭证超时时间
+     */
+    int DEFAUTL_EXPIRED_SECONDS = 3600 * 12;
+
+    /*
+    记住状态下的登录凭证超时时间
+     */
+    int REMEMBER_EXPIRED_SECONDS = 3600 * 24 * 100;
+}
