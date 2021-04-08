1bc71de8f00a3e790fe52f9867dc7b28e74cf501
wzczichen <wzcwangzichen@foxmail.com>
2021-04-08 13:47:59 +0800

    2021.4.8 解决Netty启动冲突

--- src/main/java/com/nowcoder/community/CommunityApplication.java
+++ src/main/java/com/nowcoder/community/CommunityApplication.java
@@ -3,8 +3,22 @@ package com.nowcoder.community;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class CommunityApplication {
+	/*
+	管理Bean生命周期
+	 */
+	@PostConstruct
+	public void init(){
+		/*
+		解决Netty启动冲突问题(redis和elasticsearch)
+		Netty4Utils+NettyRuntime
+		 */
+		System.setProperty("es.set.netty.runtime.available.processors","false");
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityApplication.class, args);
