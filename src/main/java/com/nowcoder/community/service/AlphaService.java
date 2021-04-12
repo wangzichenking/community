c4a342a12c7da48c968f72e8b3f7f3039db80192
wzczichen <wzcwangzichen@foxmail.com>
2021-04-12 10:40:01 +0800

    2021.4.12 JDK和Spring普通线程池与定时任务线程池

--- src/main/java/com/nowcoder/community/service/AlphaService.java
+++ src/main/java/com/nowcoder/community/service/AlphaService.java
@@ -1,8 +1,12 @@
 package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.AlphaDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -14,6 +18,9 @@ public class AlphaService {
 
     @Autowired
     private AlphaDao alphaDao;
+
+    @Autowired
+    private static final Logger LOGGER = LoggerFactory.getLogger(AlphaService.class);
 
     public AlphaService() {
 //        System.out.println("实例化AlphaService");
@@ -31,6 +38,22 @@ public class AlphaService {
 
     public String find() {
         return alphaDao.select();
+    }
+
+    /*
+    @Async：让该方法在多线程环境下，被异步的调用
+     */
+    @Async
+    public void executor1(){
+        LOGGER.debug("executor1");
+    }
+
+    /*
+    @Scheduled：让该方法定时执行
+     */
+    @Scheduled(initialDelay = 10000,fixedRate = 1000)
+    public void executor2(){
+        LOGGER.debug("executor2");
     }
 
 }
